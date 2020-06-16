c8afab3c656a1d397702ebb21b2a6c373abad230
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-06-16 18:20:19 -0500

    defined modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    loadChildren: () => import("./modules/home/home.module").then(m => m.HomeModule),
+  
+  },
+  {
+    path: "auth",
+    loadChildren: () =>
+      import("./modules/auth/auth.module").then(m => m.AuthModule)
+  },
+  {
+    path: "users",
+    loadChildren: () =>
+      import("./modules/user/user.module").then(m => m.UserModule),
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  { path: "**", redirectTo: "home", pathMatch: "full" }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      preloadingStrategy: PreloadAllModules,
+      enableTracing: false,
+      scrollPositionRestoration: 'enabled',
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
